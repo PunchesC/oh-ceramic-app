584c1de295ca09a021e12f8a7d26f537c264fc57
Curtis <c.b.punches@gmail.com>
2023-12-16 17:38:35 -0500

    first commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -7,13 +7,7 @@ import { getStorage, ref, list,listAll, getDownloadURL } from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuydredI9wg1XB9N2mRaLQpfvtqP95S-0",
-  authDomain: "olivia-hoffman-ceramics-8720e.firebaseapp.com",
-  projectId: "olivia-hoffman-ceramics-8720e",
-  storageBucket: "olivia-hoffman-ceramics-8720e.appspot.com",
-  messagingSenderId: "178282382461",
-  appId: "1:178282382461:web:a7933db8dd31dfef9b1d14",
-  measurementId: "G-R1628XPRYF"
+
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
