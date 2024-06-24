e0d31a1d0f44e6be00bb9ed109e6f4871ca492c5
Curtis <c.b.punches@gmail.com>
2024-06-24 19:12:58 -0400

    started on a cart

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomeImage from './components/home/home-image';
 import AboutMe from './components/about-me/about-me';
 import Inventory from './components/inventory/inventory';
-
+import Cart from './components/cart/cart';
 
 function App() {
-
-
   return (
-    <div className="container">
-      <nav className="navbar">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#inventory">Inventory</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-      <HomeImage />
-      <AboutMe />
-      <Inventory />
-    
-      <section id="contact">
-        <h1>Contact</h1>
-        <p className="lead">Lorem ipsum dolor si, amet consectetur adipisicing elit. Commodi, quis!</p>
-      </section>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar">
+          <ul>
+            <li><a href="#home">Home</a></li> {/* Use <a> for internal navigation */}
+            <li><a href="#about">About</a></li>
+            <li><a href="#inventory">Inventory</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><Link to="/cart">Cart</Link></li> {/* Use <Link> for different page navigation */}
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={
+            <>
+              {/* Wrap components in a fragment to display them together */}
+              <HomeImage id="home" />
+              <AboutMe id="about" />
+              <Inventory id="inventory" />
+              <section id="contact">
+                <h1>Contact</h1>
+                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, quis!</p>
+              </section>
+            </>
+          } />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
