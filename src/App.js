7f57470d041ce2de3aaf580f1de7d0b220c69555
Curtis <c.b.punches@gmail.com>
2024-07-06 13:26:56 -0400

    routing works better

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useLocation, Routes, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css';
 import HomeImage from './components/home/home-image';
 import AboutMe from './components/about-me/about-me';
@@ -7,36 +8,46 @@ import Inventory from './components/inventory/inventory';
 import Cart from './components/cart/cart';
 
 function App() {
-  return (
-    <Router>
-      <div className="container">
-        <nav className="navbar">
-          <ul>
-            <li><a href="#home">Home</a></li> {/* Use <a> for internal navigation */}
-            <li><a href="#about">About</a></li>
-            <li><a href="#inventory">Inventory</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><Link to="/cart">Cart</Link></li> {/* Use <Link> for different page navigation */}
-          </ul>
-        </nav>
+  const location = useLocation();
 
-        <Routes>
-          <Route path="/" element={
-            <>
-              {/* Wrap components in a fragment to display them together */}
-              <HomeImage id="home" />
-              <AboutMe id="about" />
-              <Inventory id="inventory" />
-              <section id="contact">
-                <h1>Contact</h1>
-                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, quis!</p>
-              </section>
-            </>
-          } />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
-    </Router>
+  useEffect(() => {
+    // Check if there's a scrollTo value in the location state
+    if (location.state?.scrollTo) {
+      const id = location.state.scrollTo;
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]); // Re-run the effect if the location changes
+
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <ul>
+          <li><Link to="/" state={{ scrollTo: 'home' }}>Home</Link></li>
+          <li><Link to="/" state={{ scrollTo: 'about' }}>About</Link></li>
+          <li><Link to="/" state={{ scrollTo: 'inventory' }}>Inventory</Link></li>
+          <li><Link to="/" state={{ scrollTo: 'contact' }}>Contact</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <HomeImage id="home" />
+            <AboutMe id="about" />
+            <Inventory id="inventory" />
+            <section id="contact">
+              <h1>Contact</h1>
+              <p className="lead">Lorem ipsum dolor sit amet...</p>
+            </section>
+          </>
+        } />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
   );
 }
 
